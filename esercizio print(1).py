27e692ed4dc70caa1dba5e578479c3faa28b7e8b
marione375 <huci.mario375@gmail.com>
2024-11-26 12:07:49 +0100

    Create esercizio print(1).py

---
+++ esercizio print(1).py
@@ -0,0 +1,13 @@
+#con il print puoi far si che il computer scriva i messaggi che è impostato come il seguente
+x = "bello"
+y = "cocco"
+print(y+' '+x)
+#ma se volessi fare dei calcoli?
+x = 121 
+y = 11
+print (x + y)
+print (x - y)
+print (x * y)
+print (x / y)
+#se volessi invece essere più semplice
+print("più semplice di così")
