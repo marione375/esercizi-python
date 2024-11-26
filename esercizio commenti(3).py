738dd3afcb0373eca962d91b20c677713d163445
marione375 <huci.mario375@gmail.com>
2024-11-26 12:10:27 +0100

    Create esercizio commenti(3).py

---
+++ esercizio commenti(3).py
@@ -0,0 +1,4 @@
+#i commenti sono questi comandi che non variano nulla a livello di codice ma servano per lo sviluppatore per orientarsi a livello di codice e per mettere delle annotazioni.
+#ce ne sono di vario tipo e nessuno di questi interferisce con i codici
+#il primo è questo asterisco
+print("hello world")
